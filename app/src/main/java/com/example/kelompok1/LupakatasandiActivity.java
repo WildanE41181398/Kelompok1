aa010b3a8e2cd7bf1088aaccf408a5b621673850
YogaE41181231 <ichayoga1@gmail.com>
2020-04-10 20:33:23 +0700

    membuat tampilan lupa kata sandi

---
+++ app/src/main/java/com/example/kelompok1/LupakatasandiActivity.java
@@ -0,0 +1,14 @@
+package com.example.kelompok1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LupakatasandiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lupakatasandi);
+    }
+}
