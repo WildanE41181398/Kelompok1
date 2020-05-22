64fe14d28ec43966e7bfbc493ddd2555be9f96b8
Primasdika Yunia Putra <dickayunia1@gmail.com>
2020-05-22 16:39:38 +0700

    Commit Tab Promosi dan Detail promosi. belum fix karena mash ada kekurangan dalam struktur database pada tabel promosi

---
+++ app/src/main/java/com/example/kelompok1/ui/promosi/DetailPromosi.java
@@ -0,0 +1,131 @@
+package com.example.kelompok1.ui.promosi;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.kelompok1.R;
+import com.squareup.picasso.Picasso;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class DetailPromosi extends AppCompatActivity {
+
+    String id_promo;
+    TextView tv_desc, tv_syarat, tv_awal, tv_akhir, tv_kode;
+    ImageView iv_promosi;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_promosi);
+        id_promo = getIntent().getStringExtra("id");
+
+        tv_desc = findViewById(R.id.tv_desc_body);
+        tv_syarat = findViewById(R.id.tv_syarat_body);
+        tv_awal = findViewById(R.id.tv_awal);
+        tv_akhir = findViewById(R.id.tv_akhir);
+        iv_promosi = findViewById(R.id.iv_detail_promosi);
+
+        getPromosibyId();
+
+    }
+
+    public void getPromosibyId(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Memuat ...");
+        progressDialog.show();
+
+            String URL_CHECK_EMAIL = "http://192.168.5.145/kelompok1_tif_d/OrenzLaundry/api/promosi/getidbyname/";
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_CHECK_EMAIL,
+                    new Response.Listener<String>() {
+                        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                        @SuppressLint("ResourceAsColor")
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response);
+                                String message = jsonObject.getString("message");
+                                progressDialog.dismiss();
+
+                                if (message.equals("success")) {
+                                    JSONArray jsonArray = jsonObject.getJSONArray("data");
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        JSONObject object = jsonArray.getJSONObject(i);
+                                        String strNama = object.getString("judul_promo").trim();
+                                        String strDesc = object.getString("deskripsi").trim();
+                                        String strSyarat = object.getString("syarat_ketentuan").trim();
+                                        String strAwal = object.getString("awal").trim();
+                                        String strAkhir = object.getString("akhir").trim();
+                                        String strStatus = object.getString("status").trim();
+                                        String strGambar = object.getString("gambar").trim();
+
+                                        Objects.requireNonNull(getSupportActionBar()).setTitle(strNama);
+                                        tv_desc.setText(strDesc);
+                                        tv_awal.setText("Awal Periode : " + strAwal.substring(0, 10));
+                                        tv_akhir.setText("Akhir Periode : " + strAkhir.substring(0, 10));
+                                        Picasso.get().load("http://192.168.5.145/kelompok1_tif_d/OrenzLaundry/assets/files/gambar_promo/" + strGambar).into(iv_promosi);
+
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                            tv_syarat.setText(Html.fromHtml(strSyarat, Html.FROM_HTML_MODE_LEGACY));
+                                        } else {
+                                            tv_syarat.setText(Html.fromHtml(strSyarat));
+                                        }
+
+                                    }
+                                } else {
+                                    Toast.makeText(DetailPromosi.this, "Tidak dapat memuat data", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                progressDialog.dismiss();
+                                Toast.makeText(DetailPromosi.this, "Error" + e.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progressDialog.dismiss();
+                            Toast.makeText(DetailPromosi.this, "Error Response" + error.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+            {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("id", id_promo);
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            requestQueue.add(stringRequest);
+    }
+}
---
+++ app/src/main/java/com/example/kelompok1/ui/promosi/RecyclerViewPromosiAdapter.java
@@ -0,0 +1,74 @@
+package com.example.kelompok1.ui.promosi;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kelompok1.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecyclerViewPromosiAdapter extends RecyclerView.Adapter<RecyclerViewPromosiAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<ModelPromosi> mData;
+
+    public RecyclerViewPromosiAdapter(Context mContext, List<ModelPromosi> mData){
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewPromosiAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.row_promosi, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, final int position) {
+        myViewHolder.akhir_priode.setText("Hingga " + mData.get(position).getAkhir_priode().substring(0, 10));
+        Picasso.get().load("http://192.168.5.145/kelompok1_tif_d/OrenzLaundry/assets/files/gambar_promo/" + mData.get(position).getGambar()).into(myViewHolder.gambar);
+
+        myViewHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, DetailPromosi.class);
+                intent.putExtra("id", mData.get(position).getId());
+                mContext.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView akhir_priode;
+        ImageView gambar;
+        LinearLayout linearLayout;
+
+        MyViewHolder(View itemview) {
+            super(itemview);
+
+            akhir_priode = itemview.findViewById(R.id.tanggal_promosi);
+            gambar = itemview.findViewById(R.id.iv_promosi);
+            linearLayout = itemview.findViewById(R.id.linear_promosi);
+        }
+    }
+
+}
